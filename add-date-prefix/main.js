818ac15f5da21d485d5e6c43067d5256a611e1ab
Jezrael Arciaga <jezraelarciaga@C02W20HFHTDG.lan>
2020-10-08 12:31:46 +0800

    Fix month, Add rename button

--- add-date-prefix/main.js
+++ add-date-prefix/main.js
@@ -12,14 +12,14 @@ define([
             return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
         };
 
+        // Add date prefix overwrite
         var handler = function () {
-            // var name = Jupyter.notebook.get_notebook_name();
-            var name = prompt();
+            var name = prompt('New notebook name');
             var has_date_prefix = /^\d+-\d+-\d+/.test(name);
             if (!has_date_prefix) {
                 var now = new Date();
                 var year = now.getFullYear();
-                var month = pad(now.getMonth(), 2);
+                var month = pad(now.getMonth()+1, 2);
                 var date = pad(now.getDate(), 2);
                 var new_name = `${year}-${month}-${date} ${name}`;
                 Jupyter.notebook.rename(new_name);
@@ -30,7 +30,7 @@ define([
         };
 
         var action = {
-            icon: 'fa-calendar-plus-o', // a font-awesome class used on buttons, etc
+            icon: 'fa-calendar-plus-o',
             help    : 'Add prefix to notebook name',
             help_index : 'zz',
             handler : handler
@@ -38,12 +38,45 @@ define([
         var prefix = 'my_extension';
         var action_name = 'add-prefix';
 
-        var full_action_name = Jupyter.actions.register(action, action_name, prefix); // returns 'my_extension:show-alert'
+        var full_action_name = Jupyter.actions.register(action, action_name, prefix);
         Jupyter.toolbar.add_buttons_group([full_action_name]);
+
+        // Rename date prefix, remove copy
+        var handler1 = function () {
+            var now = new Date();
+            var year = now.getFullYear();
+            var month = pad(now.getMonth()+1, 2);
+            var date = pad(now.getDate(), 2);
+
+            var name = Jupyter.notebook.get_notebook_name();
+            var name_regexp = /(?:(?<date>^\d+-\d+-\d+)[ -])?(?<name>.*)/;
+            var groups = name.match(name_regexp).groups;
+            var name_src = groups.name.replace(/-Copy\d+$/, '');
+
+            var new_name = `${year}-${month}-${date} ${name_src}`;
+
+            if (name != new_name) {
+                Jupyter.notebook.rename(new_name);
+                console.log(`Notebook naming fixed to ${new_name}`);
+            } else {
+                console.log('Notebook name is already correct');
+            }
+        };
+
+        var action1 = {
+            icon: 'fa-calendar-check-o',
+            help    : 'Fix prefix to notebook name',
+            help_index : 'zz1',
+            handler : handler1
+        };
+        var prefix1 = 'my_extension1';
+        var action_name1 = 'add-prefix1';
+
+        var full_action_name1 = Jupyter.actions.register(action1, action_name1, prefix1);
+        Jupyter.toolbar.add_buttons_group([full_action_name1]);
     }
 
     return {
         load_ipython_extension: load_ipython_extension
     };
-    // jupyter-notebook:rename-notebook
 });
