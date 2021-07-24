a194e1c52b596be0c6844dd0ab0734f9f4a42373
Jezrael Arciaga <jezarciaga@gmail.com>
2021-07-24 16:53:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 IFS=$'\n\t'
 
+if [[ -z "$1" ]]; then
+    echo 'usage: ./install.sh [extension]'
+    echo ''
+    echo 'Possible extensions:'
+    for var in $(ls extensions); do
+        echo "  - ${var}"
+    done
+    exit 1;
+fi
 
 extension=$1
 main_js="main"
 
 
-jupyter nbextension install $(pwd)/"$extension"/
+jupyter nbextension install $(pwd)/extensions/"$extension"/
 jupyter nbextension enable "$extension"/"$main_js"
